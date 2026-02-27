9dbb90bf046c202fcffd1c78b7a57ce899a0e468
paubib10 <psbibiloni@gmail.com>
2026-02-27 16:15:08 +0100

    created base

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById("btn-play").addEventListener("click", () => {
+  alert("¡Vamos a jugar!");
+  // Aquí lanzas la escena de juego
+});
+
+document.getElementById("btn-options").addEventListener("click", () => {
+  alert("Opciones (por implementar)");
+  // Aquí abrirías el menú de opciones
+});
