e8c48473f386cf50e8d4227374432cdf7447738e
carlosroyale <carlossegurahernando@icloud.com>
2026-04-10 13:54:00 +0200

    Añadimos todo el tema de la base de datos, usuarios, etc. Todavia no está completo

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 // index.php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // 1. Iniciamos la sesión y la conexión
 require_once 'config_sesion.php';
 
