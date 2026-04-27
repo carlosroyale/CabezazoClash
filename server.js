e00889490fa3cbf6e5960e951690e61d88cceee3
carlosroyale <carlossegurahernando@icloud.com>
2026-04-27 12:19:17 +0200

    Añadimos puntos en pantalla versus

--- server.js
+++ server.js
@@ -95,8 +95,8 @@ io.on('connection', (socket) => {
         }
     });
 });
-// const PORT = process.env.PORT || 8080;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Servidor de Matchmaking activo en el puerto ${PORT}`);
 });
