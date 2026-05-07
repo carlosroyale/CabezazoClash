1c81e59e44e9e1f67c5e6644b8da338ba33730cb
carlosroyale <carlossegurahernando@icloud.com>
2026-05-07 17:09:03 +0200

    Ajustamos un poco el bot

--- juego/js/entities.js
+++ juego/js/entities.js
@@ -379,8 +379,8 @@ function controlPlayer(p, dt, leftKey, rightKey, jumpKey, kickKey, keys, ball = 
 }
 
 const BOT_AI_CONFIG = {
-    REACTION_TIME: 0.18,
-    APPROACH_DIST: 120,
+    REACTION_TIME: 0.24,
+    APPROACH_DIST: 145,
     KICK_DIST: 90,
     KICK_COOLDOWN: 0.4,
     KICK_EXIT_DIST: 110,
@@ -453,7 +453,7 @@ class BotAIUtils {
             ballGoingWrong: ball.vx < -200,
             shouldJumpForHeader: ballIsJumpHeaderHeight &&
                 distToBall < BOT_AI_CONFIG.APPROACH_DIST &&
-                ball.vy > 0 &&
+                ball.vy > -100 &&
                 ((ball.vx > 0 && bot.x > ball.x) || (ball.vx < 0 && bot.x < ball.x)),
             shouldJumpForShot: ballIsJumpHeaderHeight && distToBall < BOT_AI_CONFIG.APPROACH_DIST,
             // Opponent-aware logic
